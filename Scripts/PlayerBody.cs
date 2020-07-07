dc03883d4805cacc19a6ce3a13c3e14c5ff46991
IndieRonin <littlebear101@gmail.com>
2020-07-07 21:46:02 +0200

    Player Movement Update

--- Scripts/PlayerBody.cs
+++ Scripts/PlayerBody.cs
@@ -5,28 +5,35 @@ public class PlayerBody : KinematicBody
 {
     //The current speed of the player
     float speed;
-    //How fast the player moves when crouched
-    float crouchMoveSpeed = 3;
     //How fast the player moves when walking 
     float walkSpeed = 7;
-    //How fast the player goes into a crouching position
-    float crouchSpeed = 26;
+    //How fast the player moves when crouched
+    float crouchSpeed = 3;
+    //How fast the player is moving when sprinting
+    float sprintSpeed = 14;
+    //How quickly the player goes into a crouched stance
+    float crouchingSpeed = 5;
+    //How quickly the pplayer acellerates
     float accelleration = 20;
+    //How har the gravity plls dow the player
     float gravity = 9.8f;
+    //The force of the jump
     float jump = 5;
-
+    //How hight the plaayer characters camera displayes when standing
     float defualtHeight = 1.5f;
+    //How high the players character camera is when it is crouvhed
     float crouchHeight = .5f;
-
-
+    //How sensitive the movement of the mouse is
     float mouseSensitivity = 0.05f;
+
 
     Vector3 direction;
     Vector3 velocity;
     Vector3 fall;
     //The spatiol object repereseting the head of the player
     Spatial head;
-    CollisionShape playerCapsule;
+    CollisionShape playerCollShape;
+    RayCast headBump;
 
     // Declare member variables here. Examples:
     // private int a = 2;
@@ -37,7 +44,8 @@ public class PlayerBody : KinematicBody
     {
         Input.SetMouseMode(Input.MouseMode.Captured);
         head = GetNode<Spatial>("Head");
-        playerCapsule = GetNode<CollisionShape>("CollisionShape");
+        playerCollShape = GetNode<CollisionShape>("CollisionShape");
+        headBump = GetNode<RayCast>("HeadBump");
     }
 
     public override void _Input(InputEvent @event)
@@ -55,64 +63,48 @@ public class PlayerBody : KinematicBody
     // Called every frame. 'delta' is the elapsed time since the previous frame.
     public override void _Process(float delta)
     {
+        bool headBumped = false;
         //Set the players speed to the walk speed by defualt
         speed = walkSpeed;
 
         direction = Vector3.Zero;
+        //Check if the head ray collider is active
+        if (headBump.IsColliding()) headBumped = true;
 
-        if (!IsOnFloor())
-        {
-            fall.y -= gravity * delta;
-        }
-        if (Input.IsActionJustPressed("Jump"))
-        {
-            fall.y = jump;
-        }
-        if (Input.IsActionJustPressed("ui_cancel"))
-        {
-            Input.SetMouseMode(Input.MouseMode.Visible);
-        }
+        if (!IsOnFloor()) fall.y -= gravity * delta;
 
-        if (Input.IsActionJustPressed("Crouch"))
-        {
-            CapsuleShape playerColShape = new CapsuleShape();
-            playerColShape.Radius = 0.5f;
-            playerColShape.Height -= crouchSpeed * delta;
-            playerColShape.Height = Mathf.Clamp(playerColShape.Height, crouchHeight, defualtHeight);
-            playerCapsule.Shape = playerColShape;
-            speed = crouchMoveSpeed;
-        }
-        else
-        {
-            CapsuleShape playerColShape = new CapsuleShape();
-            playerColShape.Radius = 0.5f;
-            playerColShape.Height -= crouchSpeed * delta;
-            playerColShape.Height = Mathf.Clamp(playerColShape.Height, crouchHeight, defualtHeight);
-            playerCapsule.Shape = playerColShape;
-        }
+        if (Input.IsActionJustPressed("Jump")) fall.y = jump;
 
-        if (Input.IsActionJustPressed("MoveForward"))
-        {
-            direction -= Transform.basis.z;
-        }
-        else if (Input.IsActionJustPressed("MoveBackward"))
-        {
-            direction += Transform.basis.z;
-        }
-        if (Input.IsActionJustPressed("MoveLeft"))
-        {
-            direction -= Transform.basis.x;
-        }
-        else if (Input.IsActionJustPressed("MoveRight"))
-        {
-            direction += Transform.basis.x;
-        }
+        if (headBumped) fall.y = -2;
 
+        if (Input.IsActionJustPressed("ui_cancel")) Input.SetMouseMode(Input.MouseMode.Visible);
+
+        //If the player is crouching and the collision shape is a capsule then 
+        if (Input.IsActionPressed("Crouch") && playerCollShape.Shape is CapsuleShape capShape)
+        {
+            capShape.Height -= crouchingSpeed * delta;
+            speed = crouchSpeed;
+        }
+        else if(!headBumped) 
+        {
+            ((CapsuleShape)playerCollShape.Shape).Height += crouchingSpeed * delta;
+            speed = walkSpeed;
+        }
+        //Clamp the max and min height for crouching when it is being modified
+        ((CapsuleShape)playerCollShape.Shape).Height = Mathf.Clamp(((CapsuleShape)playerCollShape.Shape).Height, crouchHeight, defualtHeight);
+        //If the move forward is pressed them we subtract from the transform basis z of the player body 
+        if (Input.IsActionPressed("MoveForward")) direction -= Transform.basis.z;
+        else if (Input.IsActionPressed("MoveBackward")) direction += Transform.basis.z;
+        //If the move StrafeLeft is pressed them we subtract from the transform basis x of the player body 
+        if (Input.IsActionPressed("StrafeLeft")) direction -= Transform.basis.x;
+        else if (Input.IsActionPressed("StrafeRight")) direction += Transform.basis.x;
+        //We normalize the direction as not to have faster movement when traveling diagonaly
         direction = direction.Normalized();
-
+        //We set the velocity to linearly interpolate to give us smooth acceleration and decceleration
         velocity = velocity.LinearInterpolate(direction * speed, accelleration * delta);
-
+        //Set the value from the move and slide back to the velocity vlue for the horizontal movement of the body
         velocity = MoveAndSlide(velocity, Vector3.Up);
+        //Move and slide the body to the value of the jump perameter
         MoveAndSlide(fall, Vector3.Up);
     }
 }
