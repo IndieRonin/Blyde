b84470f833d233932718f764cac595c848c3a6ea
IndieRonin <littlebear101@gmail.com>
2020-09-16 23:41:49 +0200

    Update InputHandleEvent.cs

--- Scripts/EventSystem/InputHandleEvent.cs
+++ Scripts/EventSystem/InputHandleEvent.cs
@@ -5,6 +5,6 @@ namespace EventCallback
 {
     public class InputHandleEvent : Event<InputHandleEvent>
     {
-        public bool upPressed, downPressed, leftPressed, rightPressed, lmbPressed, rmbPressed, abilityPressed, jumpPressed, sprintPressed, crouchPressed, consolePressed;
+        public bool upPressed, downPressed, leftPressed, rightPressed, lmbPressed, rmbPressed, abilityPressed, jumpPressed, sprintPressed, crouchPressed, console Pressed, escapePressed;
     }
 }
